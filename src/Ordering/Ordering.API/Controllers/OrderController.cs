59fb6456d8dc8dfae214f2883157917ddc7dd2cf
Vedat ERMİŞ <vedatermis@hotmail.com>
2020-09-30 13:08:52 +0300

    commit

---
+++ src/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController: ControllerBase
+    {
+        
+    }
+}
---
+++ src/Ordering/Ordering.Core/Entities/Base/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Core.Entities.Base
+{
+    public abstract class Entity: EntityBase<int>
+    {
+        
+    }
+}
